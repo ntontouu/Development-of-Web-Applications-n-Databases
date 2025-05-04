53c3844f20336ead8215b6dfef2bd33128bca2df
MARIA ALEXANDRA NTONTOU <maria.alexadra.ntontou@gmail.com>
2025-05-05 00:58:43 +0300

    Delete training_tracker directory

--- training_tracker/auth/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-session_start();
-session_destroy();
-header("Location: login.php");
-exit();
-?>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
