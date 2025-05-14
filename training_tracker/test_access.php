96c1aa6ffc39007ab2dcf7a4016e97dddffa153d
MARIA ALEXANDRA NTONTOU <maria.alexadra.ntontou@gmail.com>
2025-05-14 15:01:13 +0300

    Delete training_tracker directory

--- training_tracker/test_access.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'status' => 'success',
-    'path' => __FILE__,
-    'directory' => __DIR__,
-    'request_uri' => $_SERVER['REQUEST_URI'] ?? null
-]);
